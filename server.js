4cf39c33c0c76d9d59784d34bce91f23857f351e
Tomer1994Ab <Abarbanel1994@gmail.com>
2025-08-30 14:33:12 +0300

    Init a beckend toy app. add an express server and get funcs

---
+++ server.js
@@ -0,0 +1,67 @@
+import path from 'path'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+import { toyService } from './services/toy.service.js'
+import { loggerService } from './services/logger.service.js'
+
+const app = express()
+
+const corsOptions = {
+  origin: [
+    'http://127.0.0.1:8080',
+    'http://localhost:8080',
+    'http://127.0.0.1:5173',
+    'http://localhost:5173',
+  ],
+  credentials: true,
+}
+
+// Express Config:
+app.use(express.static('public'))
+app.use(cors(corsOptions))
+app.use(cookieParser())
+app.use(express.json())
+app.set('query parser', 'extended')
+
+//* REST API for "toys"
+app.get('/api/toy', (req, res) => {
+  const filterBy = {
+    txt: req.query.txt || '',
+    maxPrice: req.query.maxPrice || Infinity,
+    inStock: req.query.inStock || '',
+    labels: Array.isArray(req.query.labels)
+      ? req.query.labels
+      : typeof req.query.labels === 'string'
+      ? req.query.labels
+          .replace(/[\[\]'"]+/g, '')
+          .split(',')
+          .filter((l) => l)
+      : [],
+      sortBy: req.query.sortBy || '',
+  }
+  toyService
+    .query(filterBy)
+    .then((toys) => res.send(toys))
+    .catch((err) => {
+      loggerService.error('Cannot get toys', err)
+      res.status(400).send('cannot get toys')
+    })
+})
+
+app.get('/api/toy/:toyId', (req, res) => {
+  const { toyId } = req.params
+  toyService
+    .getById(toyId)
+    .then((toy) => res.send(toy))
+    .catch((err) => {
+      loggerService.error(`Cannot get toy ${toyId}`, err)
+      res.status(400).send(`cannot get toy ${toyId}`)
+    })
+})
+
+const PORT = process.env.PORT || 3030
+app.listen(PORT, () =>
+  loggerService.info(`Server listening on port http://127.0.0.1:${PORT}/`)
+)
---
+++ services/toy.service.js
@@ -0,0 +1,107 @@
+import fs from 'fs'
+import { utilService } from './util.service.js'
+import { loggerService } from './logger.service.js'
+import { log } from 'console'
+
+export const toyService = {
+  query,
+  getById,
+  remove,
+  save,
+}
+
+// const PAGE_SIZE = 5
+const toys = utilService.readJsonFile('data/toy.json')
+
+function query(filterBy = { txt: '' }) {
+  console.log(toys)
+  if (!toys || !toys.length) return Promise.resolve([])
+  let toysToShow = toys
+
+  //* Filter
+
+  if (filterBy.name) {
+    const regex = new RegExp(filterBy.txt, 'i')
+    toysToShow = toysToShow.filter((toy) => regex.test(toy.name))
+  }
+
+  if (filterBy.maxPrice) {
+    toysToShow = toysToShow.filter((toy) => toy.price <= filterBy.maxPrice)
+  }
+
+  if (filterBy.inStock) {
+    toysToShow = toysToShow.filter((toy) => toy.inStock)
+  }
+
+  if (filterBy.labels && filterBy.labels.length) {
+    toysToShow = toysToShow.filter((toy) =>
+      filterBy.labels.every((label) => toy.labels.includes(label))
+    )
+  }
+
+  //* Sort
+  if (filterBy.sortBy) {
+    if (filterBy.sortBy === 'name') {
+      toysToShow = toysToShow.sort((a, b) => a.name.localeCompare(b.name))
+    } else if (filterBy.sortBy === 'price') {
+      toysToShow = toysToShow.sort((a, b) => a.price - b.price)
+    } else if (filterBy.sortBy === 'createdAt') {
+      toysToShow = toysToShow.sort((a, b) => b.createdAt - a.createdAt)
+    }
+  }
+
+  // if (filterBy.pageIdx !== undefined) {
+  //     const startIdx = filterBy.pageIdx * PAGE_SIZE
+  //     toysToShow = toysToShow.slice(startIdx, startIdx + PAGE_SIZE)
+  // }
+  return Promise.resolve(toysToShow)
+}
+
+function getById(toyId) {
+  const toy = toys.find((toy) => toy._id === toyId)
+  return Promise.resolve(toy)
+}
+
+function remove(toyId, loggedinUser) {
+  const idx = toys.findIndex((toy) => toy._id === toyId)
+  if (idx === -1) return Promise.reject('No Such Toy')
+
+  const toy = toys[idx]
+  if (!loggedinUser.isAdmin && toy.owner._id !== loggedinUser._id) {
+    return Promise.reject('Not your toy')
+  }
+  toys.splice(idx, 1)
+  return _saveToysToFile()
+}
+
+function save(toy, loggedinUser) {
+  if (toy._id) {
+    const toyToUpdate = toys.find((currToy) => currToy._id === toy._id)
+    if (!loggedinUser.isAdmin && toyToUpdate.owner._id !== loggedinUser._id) {
+      return Promise.reject('Not your toy')
+    }
+    toyToUpdate.name = toy.name
+    toyToUpdate.speed = toy.speed
+    toyToUpdate.price = toy.price
+    toy = toyToUpdate
+  } else {
+    toy._id = utilService.makeId()
+    toy.owner = loggedinUser
+    toys.push(toy)
+  }
+  delete toy.owner.score
+  return _saveToysToFile().then(() => toy)
+}
+
+function _saveToysToFile() {
+  return new Promise((resolve, reject) => {
+    const data = JSON.stringify(toys, null, 2)
+    fs.writeFile('data/toy.json', data, (err) => {
+      if (err) {
+        loggerService.error('Cannot write to toys file', err)
+        return reject(err)
+      }
+      resolve()
+    })
+  })
+}
